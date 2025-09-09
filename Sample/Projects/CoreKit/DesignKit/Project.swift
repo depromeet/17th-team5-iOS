1f6a6404a4227b634f5a92df54d8cb88624e115a
DONOTINTO <goods99j@gmail.com>
2025-09-09 22:16:58 +0900

    [IC-19] snapkit 삭제

--- Sample/Projects/CoreKit/DesignKit/Project.swift
+++ Sample/Projects/CoreKit/DesignKit/Project.swift
@@ -12,7 +12,6 @@ let project = Project.configure(
     moduleType: .module(name: "DesignKit"),
     product: .framework,
     dependencies: [
-        .Library.snapKit,
     ],
     hasDemo: true
 )
