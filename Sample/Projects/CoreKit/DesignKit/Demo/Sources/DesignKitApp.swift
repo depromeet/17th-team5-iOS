1cef9110ed7f2e9c4d9c138fdbb057c8fd75f10a
leejungyeob <123792519+leejungyeob@users.noreply.github.com>
2025-09-13 21:27:42 +0900

    [IC-24] 네비게이션 디자인 시스템 작업 (#16)

--- Sample/Projects/CoreKit/DesignKit/Demo/Sources/DesignKitApp.swift
+++ Sample/Projects/CoreKit/DesignKit/Demo/Sources/DesignKitApp.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+import DesignKit
+
 @main
 struct DesignKitApp: App {
     var body: some Scene {
@@ -19,29 +21,42 @@ struct DesignKitApp: App {
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            List {
-                Section("Design System") {
-                    NavigationLink(destination: ColorView()) {
-                        HStack {
-                            Text("Colors")
-                        }
+            VStack(spacing: 0) {
+                HedgeNavigationBar(
+                    buttonText: "다음",
+                    onLeftButtonTap: {
+                        print("뒤로가기 버튼 클릭")
+                    },
+                    onRightButtonTap: {
+                        print("다음 버튼 클릭")
                     }
-                    
-                    NavigationLink(destination: FontView()) {
-                        HStack {
-                            Text("Fonts")
+                )
+                
+                List {
+                    Section("Design System") {
+                        NavigationLink(destination: ColorView()) {
+                            HStack {
+                                Text("Colors")
+                            }
                         }
-                    }
-                    
-                    NavigationLink(destination: ImageView()) {
-                        HStack {
-                            Text("Icons")
+                        
+                        NavigationLink(destination: FontView()) {
+                            HStack {
+                                Text("Fonts")
+                            }
+                        }
+                        
+                        NavigationLink(destination: ImageView()) {
+                            HStack {
+                                Text("Icons")
+                            }
                         }
                     }
                 }
             }
-            .navigationTitle("DesignKit Demo")
         }
+        .navigationBarHidden(true)
+        .ignoresSafeArea(.all)
     }
 }
 
