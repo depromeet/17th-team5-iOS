079024469a43f9e7f4816921fa627f14e4c27a71
이준영 <junlight94@gmail.com>
2025-09-20 14:11:14 +0900

    fix. swift version 6 수정 및 파일 정리

--- Hedge/Projects/Data/Networker/Sources/Bases/TargetType+.swift
+++ Hedge/Projects/Data/Networker/Sources/Bases/TargetType+.swift
@@ -1,19 +1,77 @@
 //
-//  TargetType+.swift
+//  TargetType.swift
 //  Networker
 //
-//  Created by Junyoung on 9/18/25.
-//  Copyright © 2025 HedgeCompany. All rights reserved.
+//  Created by Junyoung on 9/7/25.
+//  Copyright © 2025 SampleCompany. All rights reserved.
 //
 
 import Foundation
 
 import Alamofire
 
-public enum ContentType: String {
-    case json = "application/json"
+// MARK: - Target Type
+public protocol TargetType: URLRequestConvertible {
+    var baseURL: String { get }
+    var header: HTTPHeaders { get }
+    var method: HTTPMethod { get }
+    var path: String { get }
+    var parameters: RequestParams? { get }
+    var encoding: ParameterEncoding { get }
 }
 
+// MARK: - URLRequestConvertible
+public extension TargetType {
+    func asURLRequest() throws -> URLRequest {
+        let url =  try baseURL.asURL()
+        var urlRequest = try URLRequest(
+            url: url.absoluteString + path,
+            method: method
+        )
+        var headers = defaultHeader()
+        header.forEach {
+            headers.add($0)
+        }
+        urlRequest.headers = headers
+        
+        switch parameters {
+        case let .query(request):
+            let params = request?.toDictionary() ?? [:]
+            var components = URLComponents(string: url.appendingPathComponent(path).absoluteString)
+            
+            var queryItems: [URLQueryItem] = []
+            
+            for (key, value) in params {
+                if let arrayValue = value as? [Any] {
+                    for item in arrayValue {
+                        queryItems.append(URLQueryItem(name: key, value: String(describing: item)))
+                    }
+                } else {
+                    queryItems.append(URLQueryItem(name: key, value: String(describing: value)))
+                }
+            }
+            components?.queryItems = queryItems
+            urlRequest.url = components?.url
+            return urlRequest
+        
+        case let .body(request):
+            let params = request?.toDictionary() ?? [:]
+            urlRequest.httpBody = try JSONSerialization.data(withJSONObject: params, options: [])
+            return try encoding.encode(urlRequest, with: params)
+        
+        case let .arrayBody(request):
+            let params = request?.toArrayDictionary() ?? [[:]]
+            let jsonData = try JSONSerialization.data(withJSONObject: params, options: [])
+            urlRequest.httpBody = jsonData
+            return try encoding.encode(urlRequest, with: nil)
+        
+        case .none:
+            return urlRequest
+        }
+    }
+}
+
+// MARK: Functions
 public extension TargetType {
     // MARK: Header
     func defaultHeader() -> HTTPHeaders {
@@ -38,4 +96,8 @@ public extension TargetType {
             return JSONEncoding.default
         }
     }
+}
+
+public enum ContentType: String {
+    case json = "application/json"
 }
--- Hedge/Tuist/Package.swift
+++ Hedge/Tuist/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 @preconcurrency import PackageDescription
 
 #if TUIST
@@ -12,7 +12,7 @@ import ProjectDescriptionHelpers
             "ComposableArchitecture": .framework,
             "Kingfisher": .framework
         ],
-        baseSettings: Settings.settings(configurations: XCConfig.framework)
+        baseSettings: Settings.settings(configurations: XCConfig.library)
     )
 #endif
 
@@ -20,7 +20,7 @@ let package = Package(
     name: "TestProject",
     platforms: [.iOS(.v17)],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", exact: "5.7.1"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", exact: "5.10.2"),
         .package(url: "https://github.com/Swinject/Swinject.git", exact: "2.10.0"),
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", exact: "1.22.2"),
         .package(url: "https://github.com/onevcat/Kingfisher.git", exact: "8.5.0"),
