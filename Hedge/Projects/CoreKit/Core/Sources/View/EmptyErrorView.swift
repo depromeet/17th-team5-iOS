9cad8d2ae4b4dfe895d4ed9d5bcb25591ee51e3e
이준영 <junlight94@gmail.com>
2025-09-21 21:49:34 +0900

    feat. 주식 조회 empty 케이스 추가

---
+++ Hedge/Projects/CoreKit/Core/Sources/View/EmptyErrorView.swift
@@ -0,0 +1,36 @@
+//
+//  EmptyErrorView.swift
+//  Core
+//
+//  Created by Junyoung on 9/21/25.
+//  Copyright © 2025 HedgeCompany. All rights reserved.
+//
+
+import SwiftUI
+
+import DesignKit
+
+public struct EmptyErrorView: View {
+    public init() {}
+    
+    public var body: some View {
+        ZStack(alignment: .center) {
+            VStack(alignment: .center, spacing: 12) {
+                Image.hedgeUI.iconError
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                
+                VStack(alignment: .center, spacing: 4) {
+                    Text("검색 결과가 없어요")
+                        .font(.body1Medium)
+                        .foregroundStyle(Color.hedgeUI.textSecondary)
+                    
+                    Text("검색어를 다시 확인해보세요")
+                        .font(.body3Medium)
+                        .foregroundStyle(Color.hedgeUI.textAssistive)
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
