77d513d355439da444a7624f3fbc4c999616e7c4
이준영 <junlight94@gmail.com>
2025-10-14 00:10:56 +0900

    feat. 바텀시트 작업

--- Hedge/Projects/Features/TradeReasonFeature/Sources/Presentaition/View/TradeReasonInputView.swift
+++ Hedge/Projects/Features/TradeReasonFeature/Sources/Presentaition/View/TradeReasonInputView.swift
@@ -193,10 +193,10 @@ struct TradeReasonInputView: View {
         .onAppear {
             intent.onAppear()
         }
-        .hedgeBottomSheet(isPresented: state.isEmotionShowBinding, title: "감정 선택", maxHeight: 0.9) {
+        .hedgeBottomSheet(isPresented: state.isEmotionShowBinding, title: "감정 선택", maxHeight: 0.85) {
             EmotionBottomSheet(selection: state.emotionSelectionBinding)
         }
-        .hedgeBottomSheet(isPresented: state.isChecklistShowBinding, title: "체크리스트", maxHeight: 0.9) {
+        .hedgeBottomSheet(isPresented: state.isChecklistShowBinding, title: "체크리스트", maxHeight: 0.85) {
             AnyView(
                 state.principleBuilder.buildView(
                     principles: state.principlesBinding,
