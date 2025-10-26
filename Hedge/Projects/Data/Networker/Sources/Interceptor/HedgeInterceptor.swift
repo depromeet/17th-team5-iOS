0848727be272bdd90b92a9600b64bbb951afb4eb
이준영 <junlight94@gmail.com>
2025-10-26 20:46:02 +0900

    feat. session 분리

---
+++ Hedge/Projects/Data/Networker/Sources/Interceptor/HedgeInterceptor.swift
@@ -0,0 +1,34 @@
+//
+//  HedgeInterceptor.swift
+//  Networker
+//
+//  Created by Junyoung on 10/26/25.
+//  Copyright © 2025 HedgeCompany. All rights reserved.
+//
+
+import Foundation
+
+import Alamofire
+
+public final class HedgeInterceptor: RequestInterceptor {
+    public func adapt(
+        _ urlRequest: URLRequest,
+        for session: Session,
+        completion: @escaping (Result<URLRequest, any Error>) -> Void
+    ) {
+        var urlRequest = urlRequest
+        if let accessToken = UserDefaults.standard.string(forKey: "accessToken") {
+            urlRequest.headers["Authorization"] = "Bearer " + accessToken
+        }
+        completion(.success(urlRequest))
+    }
+    
+    public func retry(
+        _ request: Request,
+        for session: Session,
+        dueTo error: any Error,
+        completion: @escaping (RetryResult) -> Void
+    ) {
+        
+    }
+}
